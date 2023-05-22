a0174c076e14bc15caf9c5d9a677e229e7df196d
sitncrush <89580762+dvuCodes@users.noreply.github.com>
2023-05-22 14:04:25 +1000

    refractor
    Removed useEffect for the API call. Moved to API fetch to onStartQuizClick. This allows me to recall the fetch function for the onPlayAgainClick.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import Confetti from "react-confetti"
 import shuffle from "./utils/shuffle"
 
 // API :https://opentdb.com/api_config.php
+// TODO:
+// display correct answer in green and incorrect selected answer in red
+// -add media queries for larger screen sizes
+// add more comments to code
+// refractor code
 
 function App() {
   const [isStart, setIsStart] = useState(false)
@@ -15,47 +20,78 @@ function App() {
   const [fetchError, setfetchError] = useState(null)
   const [isPerfect, setisPerfect] = useState(false)
   const [score, setScore] = useState(0)
-  const [checkScore, setCheckScore] = useState(false)
+  const [gameOver, setgameOver] = useState(false)
 
-  // API fetch
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch("https://opentdb.com/api.php?amount=5")
-        if (!res.ok) {
-          throw new Error("Failed to fetch data")
-        }
-        const fetchData = await res.json()
-        const data = fetchData.results
+  // // API fetch
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await fetch("https://opentdb.com/api.php?amount=5")
+  //       if (!res.ok) {
+  //         throw new Error("Failed to fetch data")
+  //       }
+  //       const fetchData = await res.json()
+  //       const data = fetchData.results
 
-        setQuestions((oldQuestions) =>
-          data.map((question) => {
-            return {
-              question: decode(question.question),
-              answers: shuffle([
-                ...question.incorrect_answers,
-                question.correct_answer,
-              ]),
-              correct_answer: question.correct_answer,
-              selectedAnswer: "",
-              id: nanoid(),
-            }
-          })
-        )
-      } catch (err) {
-        console.log("Error fetching data:", err)
-        setfetchError("Unable to generate questions please try again")
+  //       setQuestions((oldQuestions) =>
+  //         data.map((question) => {
+  //           return {
+  //             question: decode(question.question),
+  //             answers: shuffle([
+  //               ...question.incorrect_answers,
+  //               question.correct_answer,
+  //             ]),
+  //             correct_answer: question.correct_answer,
+  //             selectedAnswer: "",
+  //             id: nanoid(),
+  //           }
+  //         })
+  //       )
+  //     } catch (err) {
+  //       console.log("Error fetching data:", err)
+  //       setfetchError("Unable to generate questions please try again")
+  //     }
+  //   }
+  //   fetchData()
+  // }, [])
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("https://opentdb.com/api.php?amount=5")
+      if (!res.ok) {
+        throw new Error("Failed to fetch data")
       }
+      const fetchData = await res.json()
+      const data = fetchData.results
+
+      setQuestions((oldQuestions) =>
+        data.map((question) => {
+          return {
+            question: decode(question.question),
+            answers: shuffle([
+              ...question.incorrect_answers,
+              question.correct_answer,
+            ]),
+            correct_answer: question.correct_answer,
+            selectedAnswer: "",
+            id: nanoid(),
+          }
+        })
+      )
+    } catch (err) {
+      console.log("Error fetching data:", err)
+      setfetchError("Unable to generate questions please try again")
     }
-    fetchData()
-  }, [])
+  }
 
   // Start game click
   const onStartQuizClick = () => {
+    fetchData()
     setIsStart(true)
     console.log(questions)
   }
 
+  // sets seleted answer
   const onHandleSelectedAnswer = (questionId, answer) => {
     setQuestions((preState) =>
       preState.map((question) => {
@@ -74,12 +110,6 @@ function App() {
   // submit answers
   const onSubmitClick = () => {
     const perfectScore = questions.length
-    if (score === perfectScore) {
-      setisPerfect(true)
-    }
-  }
-
-  useEffect(() => {
     let updatedScore = 0
 
     for (let i = 0; i < questions.length; i++) {
@@ -88,8 +118,19 @@ function App() {
       }
     }
     setScore(updatedScore)
-    setCheckScore(true)
-  }, [questions])
+
+    if (score === perfectScore) {
+      setisPerfect(true)
+    }
+
+    setgameOver(true)
+  }
+
+  const onPlayAgainClick = () => {
+    fetchData()
+    setisPerfect(false)
+    setgameOver(false)
+  }
 
   // render question components
   const renderQuestions = questions.map((question) => {
@@ -130,12 +171,28 @@ function App() {
                 <h1>Quizzical</h1>
                 {renderQuestions}
                 <div className="bottom--wrapper">
-                  <h3>
-                    Your score {score}/{questions.length}{" "}
-                  </h3>
-                  <button className="playagain--btn" onClick={onSubmitClick}>
-                    {checkScore ? "Play Again" : "Submit"}
-                  </button>
+                  {isPerfect && gameOver && (
+                    <h3>
+                      You got a perfect score!! {score}/{questions.length}
+                    </h3>
+                  )}
+                  {!isPerfect && gameOver && (
+                    <h3>
+                      Almost perfect! 🤦‍♂️ {score}/{questions.length}{" "}
+                    </h3>
+                  )}
+                  {gameOver ? (
+                    <button
+                      className="playagain--btn"
+                      onClick={onPlayAgainClick}
+                    >
+                      Play Again
+                    </button>
+                  ) : (
+                    <button className="submit--btn" onClick={onSubmitClick}>
+                      Submit
+                    </button>
+                  )}
                 </div>
               </>
             )}
@@ -147,3 +204,6 @@ function App() {
 }
 
 export default App
+
+// change text from stadnard "Your Score"
+// if score !isPerfect && gameOver
---
+++ src/utils/underLineStyle.jsx
@@ -0,0 +1,5 @@
+const underLineStyle = {
+  textDecoration: "underline",
+}
+
+export default underLineStyle
